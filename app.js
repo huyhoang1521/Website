35c6b07071ebf6394307652970d86f5275f39c43
Huy Hoang <hhoang2197@gmail.com>
2020-07-31 17:06:26 -0500

    Testing heroku build

--- app.js
+++ app.js
@@ -10,8 +10,38 @@ require("dotenv").config();
 
 const app = express();
 
-// Static folder
-app.use("/client", express.static(path.join(__dirname, "client")));
+// --> Add this
+// ** MIDDLEWARE ** //
+const whitelist = [
+  "http://localhost:3000",
+  "http://localhost:8080",
+  "https://whispering-savannah-99312.herokuapp.com",
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    console.log("** Origin of request " + origin);
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      console.log("Origin acceptable");
+      callback(null, true);
+    } else {
+      console.log("Origin rejected");
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+
+// --> Add this
+app.use(cors(corsOptions));
+
+// --> Add this
+if (process.env.NODE_ENV === "production") {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, "client/build")));
+  // Handle React routing, return all requests to React app
+  app.get("*", function (req, res) {
+    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  });
+}
 
 // Body Parser Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
