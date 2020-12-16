54d3582cf0d2815505a8f68aff2087cea348c0bb
Huy Hoang <hhoang2197@gmail.com>
2020-12-16 17:08:46 -0600

    Configured app.js for ssl certification

--- app.js
+++ app.js
@@ -41,7 +41,9 @@ app.use(cors(corsOptions));
 
 if (process.env.NODE_ENV === "production") {
   // Serve any static files
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(
+    express.static(path.join(__dirname, "client/build"), { dotfiles: "allow" })
+  );
   // Handle React routing, return all requests to React app
   app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
